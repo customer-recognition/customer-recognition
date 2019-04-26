daa61c780a5191d468ddd263250ef0d2a2e2787f
Quan <Tran.Q90@gmail.com>
2019-04-26 11:19:51 -0700

    db stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO customer (name) VALUES ('Mymy');
-INSERT INTO customer (name) VALUES ('Jacob');
-INSERT INTO customer (name) VALUES ('Nicole');
-INSERT INTO inventory (item, price) VALUES ('Hamburger', 3.00);
-INSERT INTO inventory (item, price) VALUES ('Cheeseburger', 4.00);
-INSERT INTO inventory (item, price) VALUES ('Double Double Cheeseburger', 6.00);
-INSERT INTO inventory (item, price) VALUES ('Fries', 3.00);
-INSERT INTO inventory (item, price) VALUES ('Animal-Style Fries', 4.00);
-INSERT INTO inventory (item, price) VALUES ('Soda', 2.00);
+INSERT INTO customers (name) VALUES ('Mymy');
+INSERT INTO customers (name) VALUES ('QT');
+INSERT INTO customers (name) VALUES ('DK');
+INSERT INTO inventories (item, price) VALUES ('Hamburger', 3.00);
+INSERT INTO inventories (item, price) VALUES ('Cheeseburger', 4.00);
+INSERT INTO inventories (item, price) VALUES ('Double Double Cheeseburger', 6.00);
+INSERT INTO inventories (item, price) VALUES ('Fries', 3.00);
+INSERT INTO inventories (item, price) VALUES ('Animal-Style Fries', 4.00);
+INSERT INTO inventories (item, price) VALUES ('Soda', 2.00);
