304e7719209e5e82e9fd5fd1e632c22b7c0166b1
Mymy <thoaimydinh@gmail.com>
2019-04-29 10:42:57 -0700

    canvas

--- canvas/canvas-resizing/canvas2.js
+++ canvas/canvas-resizing/canvas2.js
@@ -1,10 +1,57 @@
-var canvas = document.querySelector('canvas')
+// var canvas = document.querySelector('canvas')
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-var ctx = canvas.getContext('2d');
+// var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "skyblue";
-ctx.strokeStyle = "lightgray";
-ctx.lineWidth = 2;
+// ctx.fillStyle = "black";
+// ctx.strokeStyle = "lightgray";
+// ctx.lineWidth = 2;
+
+function tables(elem, event, area, func){
+    // area is relative to page :
+    // var area = {
+    //     x0: 60,
+    //     y0: 60,
+    //     x1: 100,
+    //     y1: 100
+    // }    
+    elem.addEventListener(event, function(e){
+        var page = {
+            x: e.pageX,
+            y: e.pageY
+        }
+        if(page.x >= area.x0 && page.x <= area.x1 && page.y >= area.y0 && page.y <= area.y1){
+            func();
+        }
+    });
+}
+
+tables(
+    document.getElementById('t1'), 'click',
+    {
+        x0: 60,
+        x1: 100,
+        y0: 60,
+        y1: 100
+    
+    },
+    function() {
+        alert('Testing!');
+        console.log('testing table 1')
+    }
+);
+
+tables (
+    document.getElementById('t2'), 'click', {
+        x0: 60,
+        x1: 100,
+        y0: 60,
+        y1: 100
+    },
+    function() {
+        alert('Testing2!');
+        console.log('testing table 2')
+    }
+)
