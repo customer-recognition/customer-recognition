9ec44aaeee433ef224a1715b08733d5c3a1e1755
David <dykim61@gmail.com>
2019-04-28 22:44:44 -0700

    attempted route

--- Login (EFFORT)/routes/api-routes.js
+++ Login (EFFORT)/routes/api-routes.js
@@ -1,6 +1,6 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
-var customerID = require("../config/customerID");
+var userID = require("../config/customerID");
 
 module.exports = function (app) {
     // Using the passport.authenticate middleware with our local strategy.
--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,8 +1,12 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    //set up the log in page//
     app.get("/", function (req, res) {
+        console.log("request")
+        res.render("login")
+    })
+    //set up the log in page//
+    app.get("/afwefa", function (req, res) {
         console.log("request")
         db.Customer.findAll({}).then(function (data) {
             var hbsObject = {
