446eaf34a72fe30d0b6dad78f7387e468bf340b8
Mymy <thoaimydinh@gmail.com>
2019-04-29 10:04:20 -0700

    sdfsf

---
+++ canvas/canvas-resizing/canvas2.js
@@ -0,0 +1,10 @@
+var canvas = document.querySelector('canvas')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "skyblue";
+ctx.strokeStyle = "lightgray";
+ctx.lineWidth = 2;
