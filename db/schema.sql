bed714c69b70954b2e31c243ba056a13e7c1ab14
David <dykim61@gmail.com>
2019-04-30 11:59:29 -0700

    utilizing bootstrap for css framework

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,3 @@
 DROP DATABASE IF EXISTS restaurant_db;
 CREATE DATABASE restaurant_db;
 USE restaurant_db;
-<<<<<<< HEAD
-
-CREATE TABLE customers
-(
-    ID INT NOT NULL AUTO_INCREMENT,
-    customer_name VARCHAR(256),
-    customer_purchases INT,
-    PRIMARY KEY(ID)
-);
-
-CREATE TABLE orders
-(
-    ID INT NOT NULL AUTO_INCREMENT,
-    order_name VARCHAR(256),
-    order_price INT,
-    order_customer INT,
-    PRIMARY KEY(ID),
-    FOREIGN KEY(order_customer) REFERENCES customers(ID)
-);
-
-CREATE TABLE users
-(
-    ID SERIAL PRIMARY KEY,
-    username VARCHAR(256),
-    PASSWORD VARCHAR(256)
-);
-=======
->>>>>>> 60ff59673d2d0738187959ffe7cd279844eaa833
