884d45f71f43bd9f563f6934b996b5305f2edf73
Quan <Tran.Q90@gmail.com>
2019-05-01 12:47:16 -0700

    deleted useless things

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -16,25 +16,6 @@ module.exports = function (app) {
         });
     });
 
-    // QT-
-    //     app.get("/customer", function(req, res){
-    //         console.log(req.query.id);
-    //         db.Customer.findAll({
-    //             where: {
-    //                 id: req.query.id
-    //             },
-    //         }).then((data)=>{
-    //             var hbsObject = {
-    //                 customer: data
-    //             };
-    //             res.render("customer", hbsObject);
-    //         }).catch((err)=>{
-    //             console.log(err);
-    //             res.status(500);
-    //         })
-    //     })
-
-
     app.get("/customer/:search", function (req, res) {
         console.log(req.params)
         db.Customer.findAll({
@@ -54,18 +35,3 @@ module.exports = function (app) {
         });
     });
 };
-
-// app.get("/", function (req, res) {
-//     db.Customer.findAll({}).then((data) => {
-//         db.Order.findAll({}).then((data2) => {
-//             var hbsObject = {
-//                 customer: data,
-//                 order: data2
-//             }
-//             res.render("index", hbsObject);
-//         })
-//     }).catch(function (err) {
-//         console.log(err);
-//         res.status(500);
-//     });
-// });
