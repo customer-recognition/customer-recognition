60c9b89326a9f20d3bab07ae15121304cfbbe6b5
Quan <Tran.Q90@gmail.com>
2019-05-01 11:59:21 -0700

    search and stuff

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -35,16 +35,37 @@ module.exports = function (app) {
     //     })
 
 
-    app.get("/customer/:customername", function (req, res) {
+    app.get("/customer/:search", function (req, res) {
+        console.log(req.params)
         db.Customer.findAll({
             where: {
-                customer_name: req.params.customername
+                customer_email: req.params.search
                 // id: req.params.customerid
             }
         }).then((data) => {
             // console.log("data: ", data);
-            res.render("customer", { Customer: data });
+            db.Order.findAll({}).then((data2) => {
+                var hbsObject = {
+                    customer: data,
+                    order: data2
+                }
+                res.render("customer", hbsObject);
+            })
         });
     });
 };
 
+// app.get("/", function (req, res) {
+//     db.Customer.findAll({}).then((data) => {
+//         db.Order.findAll({}).then((data2) => {
+//             var hbsObject = {
+//                 customer: data,
+//                 order: data2
+//             }
+//             res.render("index", hbsObject);
+//         })
+//     }).catch(function (err) {
+//         console.log(err);
+//         res.status(500);
+//     });
+// });
