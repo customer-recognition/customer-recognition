7d482b63d51b7c17ae129399aa67c4a041d93f45
Quan Tran <tran.q90@gmail.com>
2019-05-01 20:01:37 -0700

    added workign quantity column

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.post("/api/customer", function (req, res) {
+    app.put("/api/customer", function (req, res) {
         db.Customer.findAll({
             where: {
                 id: req.body.CustomerId
@@ -15,39 +15,58 @@ module.exports = function (app) {
                 }
             }]
         }).then((result) => {
-            console.log(req.body);
-            db.Customer_order.create({
-                CustomerId: req.body.CustomerId,
-                OrderId: req.body.OrderId
-            }).then((result)=>{
-                res.json(result);
+            db.Customer_order.findAll({
+                where: {
+                    CustomerId: req.body.CustomerId,
+                    OrderId: req.body.OrderId
+                }
+            }).then((result) => {
+                if (result == "") {
+                    db.Customer_order.create({
+                        CustomerId: req.body.CustomerId,
+                        OrderId: req.body.OrderId
+                    }).then((result) => {
+                        res.json(result);
+                    })
+                } else {
+                    var orderCount = result[0].dataValues.quantity;
+                    db.Customer_order.update({
+                        quantity: orderCount+=1
+                    },{
+                        where: {
+                            CustomerId: req.body.CustomerId,
+                            OrderId: req.body.OrderId
+                        }
+                    })
+                }
             })
+
         }).catch((err) => {
             console.log(err.message);
             res.status(500);
         })
     })
 
-    app.post("/api/customer/new", function(req, res){
+    app.post("/api/customer/new", function (req, res) {
         // console.log(req.body.customer_name);
         // console.log(req.body.order_id);
         db.Customer.create({
             customer_name: req.body.customer_name,
             customer_email: req.body.customer_email
-        }).then((result)=>{
+        }).then((result) => {
             res.json(result);
             console.log(result.dataValues.id);
 
             db.Customer_order.create({
                 CustomerId: result.dataValues.id,
                 OrderId: req.body.order_id
-            }).then((result)=>{
+            }).then((result) => {
                 res.json(result);
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
                 res.status(500);
             })
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
             res.status(500);
         })
@@ -66,4 +85,3 @@ module.exports = function (app) {
     //     })
     // })
 }
-
