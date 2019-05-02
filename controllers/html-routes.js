ac73413b8aaba705e4af6e89c596292550aae900
Quan Tran <tran.q90@gmail.com>
2019-05-01 22:07:46 -0700

    favorite food

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -24,12 +24,38 @@ module.exports = function (app) {
             }
         }).then((data) => {
             // console.log("data: ", data);
+            if (data != ""){
+                var customer_id = data[0].dataValues.id;
+            }
             db.Order.findAll({}).then((data2) => {
-                var hbsObject = {
-                    customer: data,
-                    order: data2
-                }
-                res.render("customer", hbsObject);
+                db.Customer.findAll({
+                    where: {
+                        id: customer_id
+                    },
+                    include: [db.Order]
+                }).then((data3)=>{
+                    var order1 = data3[0].Orders[0].dataValues.Customer_order.dataValues.quantity;
+                    var order2 = data3[0].Orders[1].dataValues.Customer_order.dataValues.quantity;
+                    var order3 = data3[0].Orders[2].dataValues.Customer_order.dataValues.quantity;
+                
+                    if (order1 >= order2 && order1 >= order3){
+                        var favorite_food = data3[0].Orders[0].dataValues.order_name
+                    } else if (order2 >= order1 && order1 >= order3){
+                        var favorite_food = data3[0].Orders[1].dataValues.order_name
+                    } else if (order3 >= order1 && order3 >= order2){
+                        var favorite_food = data3[0].Orders[2].dataValues.order_name
+                    }
+
+                    var hbsObject = {
+                        customer: data,
+                        order: data2,
+                        favorite_food: favorite_food,
+                        customer_order: data3
+                    }
+                    var wtf = hbsObject.customer_order[0].dataValues.Orders[0].dataValues.Customer_order.dataValues;
+                    console.log(wtf);
+                    res.render("customer", hbsObject);
+                })
             })
         });
     });
